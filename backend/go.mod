0d9e5887b209f7d0c5736e084a9159fb3dd0b1eb
triravi <ravi.tripathi@zeoauto.in>
2023-09-25 22:51:03 +0530

    first commit

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/B-Dollar-C/chat
+
+go 1.17
+
+require github.com/gorilla/websocket v1.5.0
---
+++ backend/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/B-Dollar-C/chat/pkg/websocket"
+	"net/http"
+)
+
+func serveWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Websocket Endpoint Reached")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v/n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Chat With Us")
+	setupRoutes()
+	http.ListenAndServe(":9000", nil)
+
+}
